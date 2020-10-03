bf5b6a341525e00a5aab083a5cbc75bb0170c60e
Gonçalo Teixeira <up201806562@fe.up.pt>
2020-10-03 17:53:38 +0100

    views - fixed a bug on provided code

--- TP1/MySceneGraph.js
+++ TP1/MySceneGraph.js
@@ -877,12 +877,12 @@ class MySceneGraph {
 
 
     parseBoolean(node, name, messageError){
-        var boolVal = true;
-        boolVal = this.reader.getBoolean(node, name);
-        if (!(boolVal != null && !isNaN(boolVal) && (boolVal == true || boolVal == false)))
+        let boolVal = this.reader.getBoolean(node, name);
+        if (!(boolVal != null && !isNaN(boolVal) && (boolVal == true || boolVal == false))) {
             this.onXMLMinorError("unable to parse value component " + messageError + "; assuming 'value = 1'");
-
-        return boolVal || 1;
+            boolVal = true
+        }
+        return boolVal;
     }
     /**
      * Parse the coordinates from a node with ID = id
