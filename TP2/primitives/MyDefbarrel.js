a4710f776d093dd0426613313a1bbaec684391e0
Andre Gomes <up201806224@fe.up.pt>
2020-11-18 11:35:30 +0000

    vars diferentes

--- TP2/primitives/MyDefbarrel.js
+++ TP2/primitives/MyDefbarrel.js
@@ -6,37 +6,40 @@ class MyDefbarrel extends CGFobject {
 
         this.updatedTexCoords = true; // no need for updateTexCoords
 
-        const H = (4/3) * (middle - base)
-        const h = (4/3) * base
+        const r = base
+        const R = middle
+        const L = height
+        const H = (4/3) * (R - r)
+        const h = (4/3) * r
 
-        let angle = Math.atan(h/base)
-        let aux = height / Math.tan(Math.PI / 2.5)
+        let angle = Math.atan(h/r) // atan(4/3)
+        let aux = L / Math.tan(Math.PI / 2.5)
 
         this.surface = new Patch(scene, stacks, slices, 3, 3,
             [
                     [
-                        [-base, 0, height, 1],
-                        [-base - H, 0, height - aux , 1],
-                        [-base - H, 0, aux, 1],
-                        [-base, 0, 0, 1],
+                        [-r, 0, L, 1],
+                        [-(r + H), 0, L - aux , 1],
+                        [-(r + H), 0, aux, 1],
+                        [-r, 0, 0, 1],
                     ],
                     [
-                        [-base, h, height, 1],
-                        [(base + H - base) * Math.cos(Math.PI - angle) - base, Math.sin(Math.PI - angle) * (base + h) + base, height - aux , 1],
-                        [(base + H - base) * Math.cos(Math.PI - angle) - base, Math.sin(Math.PI - angle) * (base + h) + base, aux, 1],
-                        [-base, h, 0, 1],
+                        [-r, h, L, 1],
+                        [(H) * Math.cos(Math.PI - angle) - r, Math.sin(Math.PI - angle) * (r+h) + r, L - aux , 1],
+                        [(H) * Math.cos(Math.PI - angle) - r, Math.sin(Math.PI - angle) * (r+h) + r, aux, 1],
+                        [-r, h, 0, 1],
                     ],
                     [
-                        [base, h, height, 1],
-                        [(base + H - base) * Math.cos(angle) + base, Math.sin(angle) * (base + h) + base, height - aux , 1],
-                        [(base + H - base) * Math.cos(angle) + base, Math.sin(angle) * (base + h) + base, aux, 1],
-                        [base, h, 0, 1],
+                        [r, h, L, 1],
+                        [(H) * Math.cos(angle) + r, Math.sin(angle) * (r+h) + r, L - aux , 1],
+                        [(H) * Math.cos(angle) + r, Math.sin(angle) * (r+h) + r, aux, 1],
+                        [r, h, 0, 1],
                     ],
                     [
-                        [base, 0, height, 1],
-                        [base + H, 0, height - aux , 1],
-                        [base + H, 0, aux, 1],
-                        [base, 0, 0, 1],
+                        [r, 0, L, 1],
+                        [r + H, 0, L - aux , 1],
+                        [r + H, 0, aux, 1],
+                        [r, 0, 0, 1],
                     ],
             ])
     }
