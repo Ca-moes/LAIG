ae37f2d60393d85805366fff8bde24efbf09ee26
Gonçalo Teixeira <up201806562@fe.up.pt>
2020-12-07 18:17:18 +0000

    added time calculations to solve a bug

--- TP3/game/GameState/ReadyState.js
+++ TP3/game/GameState/ReadyState.js
@@ -4,17 +4,25 @@ class ReadyState extends GameState {
     }
 
     pickTile(tile) {
+        let start = Date.now()
         let reply = this.orchestrator.prolog.canPickTile(tile)
+        console.log("canPickTile(): " + (Date.now() - start).toString())
 
         if (reply === 0) {
+            start = Date.now()
             tile.pickPiece()
             tile.highlightTile()
-
             this.orchestrator.startPicking(tile)
+            console.log("startPicking(): " + (Date.now() - start).toString())
 
             // highlight possible movements
+            start = Date.now()
             let reply = this.orchestrator.prolog.getPossibleTiles(tile)
+            console.log("getPossibleTiles(): " + (Date.now() - start).toString())
+
+            start = Date.now()
             if (reply instanceof Array) this.orchestrator.gameboard.highlightEnemyTiles(reply)
+            console.log("highlightTiles(): " + (Date.now() - start).toString())
 
             this.orchestrator.changeState(new MoveState(this.orchestrator))
         }
