5d217aa7a3cb77b97d4ad1f468abb8441857d658
Gonçalo Teixeira <up201806562@fe.up.pt>
2021-01-03 10:50:08 +0000

    MyAuxiliaryBoard documentation, removed MyBox

--- TP3/game/MyAuxiliaryBoard.js
+++ TP3/game/MyAuxiliaryBoard.js
@@ -1,4 +1,13 @@
+/**
+ * Class to be responsible for holding the pieces after they are
+ * removed from the board
+ */
 class MyAuxiliaryBoard extends CGFobject {
+    /**
+     * Starts the auxiliary board
+     * @param {XMLscene} scene
+     * @param {MyGameBoard} gameboard
+     */
     constructor(scene, gameboard) {
         super(scene);
         this.scene = scene
@@ -9,10 +18,17 @@ class MyAuxiliaryBoard extends CGFobject {
         this.pieces = []
     }
 
+    /**
+     * empties the board, right?
+     */
     emptyBoard() {
         this.pieces = []
     }
 
+    /**
+     * This algorithm finds where is position the last inserted piece
+     * @returns {{x: number, y: number, z: number}}
+     */
     currentCoords() {
         let x = 0, y = 0, z = 0;
         if (this.pieces.length <= 0) return {x: x, y: y, z: z}
@@ -30,14 +46,26 @@ class MyAuxiliaryBoard extends CGFobject {
         return {x: x, y: y, z: z}
     }
 
+    /**
+     * This method removes the last inserted piece
+     */
     undo() {
         this.pieces.pop()
     }
 
+    /**
+     * This method inserts a piece on the stack
+     * @param {MyPiece} piece piece to insert
+     */
     addPiece(piece) {
         this.pieces.push(piece)
     }
 
+    /**
+     * This method is similar to the currentCoords, but it this one finds where
+     * the next (not inserted yet) piece should be positioned on the stack.
+     * @returns {{x: number, y: number, z: number}}
+     */
     getNextPieceCoords() {
         let x = 0, y = 0, z = 0;
         for (let i = 0; i < this.pieces.length; i++) {
@@ -54,6 +82,11 @@ class MyAuxiliaryBoard extends CGFobject {
         return {x: x, y: y, z: z}
     }
 
+    /**
+     * This method displays the auxiliary board, it displays the pieces according to
+     * a position calculated here, and the it applies the box material and displays the
+     * box model.
+     */
     display() {
         this.scene.pushMatrix()
         let x = 0, y = 0, z = 0;
@@ -78,6 +111,10 @@ class MyAuxiliaryBoard extends CGFobject {
         this.scene.popMatrix()
     }
 
+    /**
+     * This method updates every piece on the auxiliary board
+     * @param t time in seconds
+     */
     update(t) {
         this.pieces.forEach(piece => piece.update(t))
     }
