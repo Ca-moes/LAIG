500d39f90f10e207f19aa0a36e01b4516154688b
Gonçalo Teixeira <up201806562@fe.up.pt>
2021-01-03 11:53:23 +0000

    BoardAnimationState documentation

--- TP3/game/GameState/BoardAnimationState.js
+++ TP3/game/GameState/BoardAnimationState.js
@@ -3,6 +3,10 @@ class BoardAnimationState extends GameState {
         super(orchestrator);
     }
 
+    /**
+     * Starts an Animation for the whole board
+     * @param {String} type
+     */
     startAnimation(type) {
         if (type === "start")
             this.orchestrator.gameboard.startAppearingAnimation(() => { this.animationEnd() })
@@ -12,6 +16,9 @@ class BoardAnimationState extends GameState {
             this.orchestrator.gameboard.startRestartAnimation(() => { this.orchestrator.onReplayAnimationCompleted() })
     }
 
+    /**
+     * After the board animation is done, it changes the state to ready state.
+     */
     animationEnd() {
         this.orchestrator.startTime = Date.now() / 1000
         this.orchestrator.moveStartTime = Date.now() / 1000
@@ -19,18 +26,19 @@ class BoardAnimationState extends GameState {
     }
 
     continue() {
-
+        // cannot continue on board animation state
     }
 
     pause() {
-
+        // cannot pause on board animation state
     }
 
     pickTile(tile) {
-
+        // cannot pick a tile on board animation state
     }
 
     update(time) {
+        // calls the update (paused version of it so the time is not changed)
         this.pauseUpdate(time)
     }
 }
